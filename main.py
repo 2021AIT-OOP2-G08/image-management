01bf2a2886dfb7293796957b1b7db4b1e8fe7756
Ren159 <k20007kk@aitech.ac.jp>
2022-01-13 15:14:15 +0900

    formの名前をditailに変更
    cssの文字のフォントをヒラギノ角ゴに、バックグラウンドのサイズや色の指定

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def Home():
 #画像アップロード、csv書き込み
 @app.route('/upload', methods=["GET",'POST'])
 def up():
+    """
     #データの登録処理
     prefecture_name = request.form.get("im",None)
     image_pass = request.form.get("imp",None)
@@ -46,6 +47,7 @@ def up():
 
     with open('image.csv',"w") as f:
         csv.dump(data,f,indent =3)
+        """
         
     return render_template('upload.html', message='アップロードに成功しました。')
   
